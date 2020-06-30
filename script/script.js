1c25988f0171da9bb81931ebe7e3a367afbecd3a
aldin-nasrun <adnzain40@gmail.com>
2020-06-30 23:39:02 +0700

    add js annimation

--- script/script.js
+++ script/script.js
@@ -2,8 +2,29 @@
 let randomKey = Math.floor(Math.random() * 10);
 let numkey = randomKey;
 //array to card
-const quoteText = (document.querySelector("#quote").innerHTML = quote[numkey]);
-const quoteName = (document.querySelector(
-  "#name"
-).innerHTML = `-${name[numkey]}`);
+const quoteText = quote[numkey];
+const quoteName = `-${name[numkey]}`;
 //console.log(quote[randomKey]);
+var arrayQuote = quoteText.split("");
+var arrayName = quoteName.split("");
+var timer;
+
+function frameLooper() {
+  if (arrayQuote.length > 0) {
+    document.getElementById("quote").innerHTML += arrayQuote.shift();
+  } else {
+    clearTimeout(timer);
+  }
+  loopTimer = setTimeout("frameLooper()", 70); /* change 70 for speed */
+}
+frameLooper();
+
+function frameLooper2() {
+  if (arrayName.length > 0) {
+    document.getElementById("name").innerHTML += arrayName.shift();
+  } else {
+    clearTimeout(timer);
+  }
+  loopTimer = setTimeout("frameLooper2()", 200); /* change 70 for speed */
+}
+frameLooper2();
